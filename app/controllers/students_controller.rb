6a56edcfe56ee34be0555d47c9ee3d45f374cb05
Axel Straube & David Garber <student@epicodus.com>
2015-07-01 10:31:25 -0700

    Add a stud(ent)'s name.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,22 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
     render :show
   end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to students_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name)
+  end
 end
